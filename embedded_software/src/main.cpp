c32981d17ff9f6f9b7e6b152dfa7e721423eb9b1
Miller <bem002@ucsd.edu>
2026-05-09 00:24:49 -0700

    finished, still needs tweaking

--- embedded_software/src/main.cpp
+++ embedded_software/src/main.cpp
@@ -1,20 +1,29 @@
 #include "soc/gpio_struct.h"
 #include "esp_rom_sys.h"
+#include "esp_rom_uart.h"
+#include "../vis_ssh_on/vis_ssh_on.hpp"
 
 #define LED_PIN 13
 
-// Bare metal C code to blink LED
+vis_ssh_on vis;
+
 extern "C" void app_main(void) {
-    esp_rom_delay_us(2000000); // <---- DO NOT DELETE
-    
-    // Un-comment code to remotely make LED blink!
-    /*
+    esp_rom_delay_us(2000000); // DO NOT DELETE
+
+    // Configure GPIO13 as output
     GPIO.enable_w1ts = (1 << LED_PIN);
+
+    esp_rom_printf("Starting LED blink program...\n");
+
     while (1) {
+        vis.test_out("LED ON\n");
         GPIO.out_w1ts = (1 << LED_PIN);
+
         esp_rom_delay_us(900000);
+
+        vis.test_out("LED OFF\n");
         GPIO.out_w1tc = (1 << LED_PIN);
+
         esp_rom_delay_us(900000);
     }
-    */
 }
